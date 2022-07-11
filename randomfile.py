a779492463c661d9b23ba5b08208fc4cadd92b59
Rizel Scarlett <blackgirlbytes@github.com>
2022-07-11 16:49:57 -0400

    Update randomfile.py

--- randomfile.py
+++ randomfile.py
@@ -1,1 +1,8 @@
-print('Hello world')
+import sys
+import tweepy
+client = tweepy.Client(consumer_key=sys.argv[1],
+                       consumer_secret=sys.argv[2],
+                       access_token=sys.argv[3],
+                       access_token_secret=sys.argv[4])
+response = client.create_tweet(text='test') 
+print(response)
