188bb685a8d850d9c39905942310d257f41d573a
Rizel Scarlett <blackgirlbytes@github.com>
2022-07-11 16:57:31 -0400

    Update randomfile.py

--- randomfile.py
+++ randomfile.py
@@ -4,5 +4,5 @@ client = tweepy.Client(consumer_key=sys.argv[1],
                        consumer_secret=sys.argv[2],
                        access_token=sys.argv[3],
                        access_token_secret=sys.argv[4])
-response = client.create_tweet(text='test') 
+response = client.create_tweet(text='ignore tweet. testing.') 
 print(response)
