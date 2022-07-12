9c6afdb8b71ea535aa2eb58544c271c1f34bd300
Rizel Scarlett <blackgirlbytes@github.com>
2022-07-11 22:51:56 -0400

    Update randomfile.py

--- randomfile.py
+++ randomfile.py
@@ -4,5 +4,5 @@ client = tweepy.Client(consumer_key=sys.argv[2],
                        consumer_secret=sys.argv[3],
                        access_token=sys.argv[4],
                        access_token_secret=sys.argv[5])
-response = client.create_tweet(text="good first issue - ", sys.argv[1]) 
+response = client.create_tweet(text=sys.argv[1]) 
 print(response)
