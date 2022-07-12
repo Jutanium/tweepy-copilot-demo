4aa72818e5b8c4dc09e6c9dd580713016727eaae
Rizel Scarlett <blackgirlbytes@github.com>
2022-07-11 23:30:04 -0400

    Update randomfile.py

--- randomfile.py
+++ randomfile.py
@@ -1,10 +1,10 @@
 import sys
 import tweepy
-client = tweepy.Client(consumer_key=sys.argv[2],
-                       consumer_secret=sys.argv[3],
-                       access_token=sys.argv[4],
-                       access_token_secret=sys.argv[5])
-url=sys.argv[1]
+client = tweepy.Client(consumer_key=sys.argv[1],
+                       consumer_secret=sys.argv[2],
+                       access_token=sys.argv[3],
+                       access_token_secret=sys.argv[4])
+url=sys.argv[5]
 annoucement="this is a good first issue "
 response = client.create_tweet(text=annoucement + url) 
 print(response)
